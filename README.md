e67a675188afdc3675f9ad81b0eacd19bbd10183
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2022-09-08 00:08:26 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# My personal page
+This page summarizes my doings and shenanigans related to software engineering. Fell free to explore and adapt the page to your liking.
+Pull requests and issues are appreciated.
+
+Developed with:
+- Svelte;
+- Vite;
+- TailWind;
+- P3.js;
+
+Felipe J. O. Ribeiro
---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './components/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1 class="text-3xl">Never before</h1>
+
+  <Counter />
+
+  <p>
+    My portfolio with my projects
+  </p>
+</main>
